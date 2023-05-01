7d293eced6d96f9d5f507deff0a941e3d4d2a569
Jacob Pixler <JacobP@facility.inventive-group.com>
2023-05-01 16:59:32 -0600

    Updated new order UI

--- src/pages/start-new-order.jsx
+++ src/pages/start-new-order.jsx
@@ -19,7 +19,7 @@ const startNewOrder = () => {
 
   return (
     <Layout title={"Gena | New Order"}>
-        <div className={"flex flex-col p-20"}>
+      <div className={"flex flex-col p-20"}>
         <div className='flex items-end'>
           <div className='mr-auto'><h1 className='text-3xl font-medium font-genaPrimary'>Labels</h1></div>
           <div className='flex justify-end items-end gap-5 w-2/5'>
