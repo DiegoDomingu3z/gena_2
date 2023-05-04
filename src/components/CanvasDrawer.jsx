4e95d7c75980c0ad1854586daebd20f36d58a683
Jacob Pixler <JacobP@facility.inventive-group.com>
2023-05-04 09:06:35 -0600

    Adding canvas Drawer

--- src/components/CanvasDrawer.jsx
+++ src/components/CanvasDrawer.jsx
@@ -6,7 +6,7 @@ const CanvasDrawer = () => {
     const { toggleCanvasDrawer, setToggleCanvasDrawer } = useCanvasDrawer();
 
   return (
-    <div className={toggleCanvasDrawer ? "flex absolute w-72 z-40 bg-black h-screen" : "hidden"}>CanvasDrawer</div>
+    <div className={toggleCanvasDrawer ? "flex absolute w-72 z-40 bg-black h-screen" : "hidden"}>CanvasDrawersfdsdf</div>
   )
 }
 
