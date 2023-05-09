ba0dc45fd17e0f97947a2c0776a7e88dddd425e4
Jacob Pixler <JacobP@facility.inventive-group.com>
2023-05-09 09:04:00 -0600

    Moved Spending Component to PrinShopView folder

--- src/components/CartCanvasDrawer.jsx
+++ src/components/CartCanvasDrawer.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
-import { useCanvasDrawer } from '~/Contexts/canvasDrawerContext'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
 
 
 const CartCanvasDrawer = ({toggleCartCanvas, setToggleCartCanvas}) => {
 
   return (
-    <div className={toggleCartCanvas ? 'pt-5 top-0 right-0 w-56 bg-[#233043] drop-shadow-2xl fixed h-full z-40 ease-in-out duration-500 flex flex-col' : "pt-5 top-0 w-56 bg-[#233043] drop-shadow-2xl fixed h-full z-40 ease-in-out duration-500 flex flex-col -right-full"}>
-        <button className='text-white' onClick={() => setToggleCartCanvas(!toggleCartCanvas)}>X</button>
-      <div className='w-full flex justify-center mb-20'>
-        <p className="text-white">Display orders and whatnot here</p>
+    <div className={toggleCartCanvas ? 'pt-5 top-0 right-0 w-96 bg-white drop-shadow-2xl fixed h-full z-40 ease-in-out duration-500 flex flex-col' : "pt-5 top-0 w-96 bg-white drop-shadow-2xl fixed h-full z-40 ease-in-out duration-500 flex flex-col -right-full"}>
+      <div className='w-full flex flex-col justify-center'>
+        <button onClick={() => setToggleCartCanvas(!toggleCartCanvas)} className='border transition-all ease-in-out duration-300 hover:scale-110 bg-[#233043] hover:bg-white hover:text-inherit mb-10 w-3/4 self-center rounded-md h-10 text-white text flex items-center justify-center gap-5'><FontAwesomeIcon className='text-xl' icon={faArrowRightLong} /> Close</button>
       </div>
+      <CartCanvasLabelCard />
     </div>
   )
 }
 
 export default CartCanvasDrawer
+
+
+
+const CartCanvasLabelCard = () => {
+  return (
+    <div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
