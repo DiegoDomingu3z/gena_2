5c74a79bcbf4d3d0b3d3c33b1a619aac34ec4f1c
Diego Dominguez <DiegoD@facility.inventive-group.com>
2023-05-09 10:22:01 -0600

    Merged conflicts

--- src/components/CartCanvasDrawer.jsx
+++ src/components/CartCanvasDrawer.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useCanvasDrawer } from '~/Contexts/canvasDrawerContext'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmarkCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRightLong, faTrash } from '@fortawesome/free-solid-svg-icons'
@@ -15,24 +14,11 @@ const CartCanvasDrawer = ({ toggleCartCanvas, setToggleCartCanvas }) => {
         <button onClick={() => setToggleCartCanvas(!toggleCartCanvas)} className='border transition-all ease-in-out duration-300 hover:scale-110 bg-[#233043] hover:bg-white hover:text-inherit mb-10 w-3/4 self-center rounded-md h-10 text-white text flex items-center justify-center gap-5'><FontAwesomeIcon className='text-xl' icon={faArrowRightLong} /> Close</button>
       </div>
       <CartCanvasLabelCard />
-      {/* <div className={toggleCartCanvas ? 'pt-5 top-0 right-0 w-72 bg-[#233043] drop-shadow-2xl fixed h-full z-40 ease-in-out duration-700 flex flex-col ' : "pt-5 top-0 w-56 bg-[#233043] drop-shadow-2xl fixed h-full z-40 ease-in-out duration-500 flex flex-col -right-full"}>
-      <div className='relative h-full'>
-        <div className='flex justify-between mx-4'>
-          <span className='text-white text-3xl mt-1 font-semibold'>
-            Label Cart
-          </span>
-          <button className='text-white ' onClick={() => setToggleCartCanvas(!toggleCartCanvas)}>
-            <FontAwesomeIcon className='p-3 rounded-full drop-shadow-sm
-       bg-[#233042] text-white' icon={faXmarkCircle} size='xl' /></button>
+      <div className='absolute bottom-0 '>
+        <div className='mb-3 m-2 mx-5  rounded-lg w-64 h-48'>
+          <textarea className='rounded-lg bg-[#233042] text-white p-5' placeholder='Notes...' name="" id="" cols="39" rows="6"></textarea>
         </div>
-        <div className='w-full flex justify-center mb-20'>
-          <p className="text-white">Display orders and whatnot here</p>
-        </div>
-        <div className='absolute bottom-0 '>
-          <div className='mb-3 m-2 mx-5  rounded-lg w-64 h-48'>
-            <textarea className='rounded-lg' name="" id="" cols="32" rows="8"></textarea>
-          </div>
-        </div> */}
+      </div>
     </div>
   )
 }
