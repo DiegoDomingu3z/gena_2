55d5a4ade5ae68101d590ec9d740d9795ded133f
Jacob Pixler <JacobP@facility.inventive-group.com>
2023-08-07 13:36:09 -0600

    Updated sorting for materials

--- src/components/PrintShopView/NewMaterial.jsx
+++ src/components/PrintShopView/NewMaterial.jsx
@@ -3,15 +3,20 @@ import { Formik, Field, Form } from 'formik'
 import { useState, useEffect } from 'react';
 import { createNewMaterial, getMaterials } from '../../../store/Material/Thunks';
 import { useDispatch } from 'react-redux'
+import Swal from 'sweetalert2'
+
 
 const NewMaterial = () => {
     const dispatch = useDispatch();
     const [individualMatCard, setIndividualMatCard] = useState([]);
+    const [materialsArray, setMaterialsArray] = useState([]);
 
     const fetchMaterials = async () => {
         const materials = await dispatch(getMaterials());
         const materialsArray = materials.payload;
-        const mappedMaterials = materialsArray.map((material) => {
+        const sortedMaterials = [...materialsArray].sort((a, b) => a.name.localeCompare(b.name));
+        setMaterialsArray(sortedMaterials);
+        const mappedMaterials = sortedMaterials.map((material) => {
           return <MaterialCard key={material._id} material={material} />;
         });
   
@@ -30,6 +35,46 @@ const NewMaterial = () => {
     )
 }
 
+const successToast = async () => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'center',
+      iconColor: 'white',
+      customClass: {
+        popup: 'colored-toast',
+        container: 'addToCartToast',
+      },
+      showConfirmButton: false,
+      timer: 1500,
+      timerProgressBar: true
+    })
+    await Toast.fire({
+      icon: 'success',
+      title: 'Material Added!'
+    })
+  }
+  const failureToast = async () => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'center',
+      iconColor: 'orange',
+      customClass: {
+        popup: 'colored-toast',
+        container: 'addToCartToast',
+      },
+      showConfirmButton: false,
+      timer: 1500,
+      timerProgressBar: true
+    })
+    await Toast.fire({
+      icon: 'warning',
+      title: 'Material Already Exists!'
+    })
+  }
+
+  const dataForSubmission = {
+    name: ''
+  }
 
   return (
       <div className={"flex flex-col pt-20 pr-20 pl-20"}>
@@ -40,14 +85,20 @@ const NewMaterial = () => {
       <div className={'flex gap-10 flex-col items-center overflow-auto h-[90rem] laptop:h-[44.5rem] pb-5'}>
           <div className={'laptop:w-3/5 w-4/5 self-center justify-self-center bg-white rounded-xl p-5 drop-shadow-lg'}>
           <Formik
-                        initialValues={{
-                            name: ''
-                        }}
+                        initialValues={dataForSubmission}
                         onSubmit={async (values, helpers) => {
                             const token = sessionStorage.getItem('accessToken');
-                            dispatch(createNewMaterial({token, values}))
-                            fetchMaterials();
-                            helpers.resetForm();
+                            console.log(materialsArray)
+                            const foundMatch = materialsArray.some(v => v.name == values.name)
+                            console.log(foundMatch)
+                            if(!foundMatch){
+                                dispatch(createNewMaterial({token, values}))
+                                fetchMaterials();
+                                successToast()
+                                helpers.resetForm();
+                                return
+                            }
+                            failureToast();
                         }}
                     >
                         {({ isSubmitting }) => (
