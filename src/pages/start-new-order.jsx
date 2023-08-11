2adf839a7ef8bc95a811c2b9ef235704c0fbe3cc
Jacob Pixler <JacobP@facility.inventive-group.com>
2023-08-11 16:02:57 -0600

    fixed merge conflict

--- src/pages/start-new-order.jsx
+++ src/pages/start-new-order.jsx
@@ -65,13 +65,8 @@ const startNewOrder = () => {
 
   return (
     <Layout title={"Gena | New Order"}>
-<<<<<<< HEAD
-      <CartCanvasDrawer toggleCartCanvas={toggleCartCanvas} setToggleCartCanvas={setToggleCartCanvas} />
-      <div className={"flex flex-col p-20"}>
-=======
       <CartCanvasDrawer toggleCartCanvas={toggleCartCanvas} setToggleCartCanvas={setToggleCartCanvas} render={render} />
-      <div className={"flex flex-col pt-20 pr-20 pl-20"}>
->>>>>>> 2bd92250048bbebebed8c741e99855baef643f47
+      <div className={"flex flex-col p-20"}>
         <div className='flex items-end '>
           <div className='mr-auto'><h1 className='text-3xl font-medium font-genaPrimary'>Labels</h1></div>
           <div className='flex justify-end items-end gap-5 w-2/5'>
