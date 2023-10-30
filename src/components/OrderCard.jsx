abb1f912fc45d9898b51a065ee81865e01f4fcec
Fernando Figueiredo <bookaneers@gmail.com>
2023-10-30 14:43:36 -0600

    pushing 10/30/23 changes

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -151,7 +151,7 @@ const OrderCard = ({ deleted, setDeleted, modalState, setModalState, blobs, setB
                     setBlobs([])
                     handleUpdateModal(o._id)
                   }}
-                  data-order-id={o._id} className={`text-[#233043] hover:bg-[#ff9800] hidden lg:block hover:text-white transition-all ease-in-out w-7 h-7 rounded-full ${(o.status === 'delivered') && 'pointer-events-none'}`}>
+                  data-order-id={o._id} className={`text-[#233043] hover:bg-[#ff9800] hidden lg:block hover:text-white transition-all ease-in-out w-7 h-7 rounded-full`}>
                   <FontAwesomeIcon icon={faPencil} /></button>
               </Tooltip>
               <Tooltip placement='top' title="Delete Order">
