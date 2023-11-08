1e086e6ec5c0474f14d93ca69bbc97ff910aca16
Fernando Figueiredo <bookaneers@gmail.com>
2023-11-08 11:32:07 -0700

    pushing 11/8/23 logs

--- src/components/PrintShopView/ProcessingOrders.jsx
+++ src/components/PrintShopView/ProcessingOrders.jsx
@@ -75,8 +75,12 @@ const ProcessingOrders = ({ deliverMultipleOrders, setDeliverMultipleOrders }) =
 
     const sanitizePath = (path) => {
         // MIGHT HAVE TO CHANGE IN THE FUTURE
-        let realPath = path.slice(47)
-        return `${realPath}`
+        if (path) {
+            let realPath = path.slice(47)
+            return `${realPath}`
+        } 
+        
+        return ""
     }
 
     const openFileManager = async (id) => {
