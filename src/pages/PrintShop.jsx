18d96503fd955650cd62d49f4c01739e95344b60
Fernando Figueiredo <bookaneers@gmail.com>
2023-11-14 10:42:00 -0700

    Area options now required and socket implemented

--- src/pages/PrintShop.jsx
+++ src/pages/PrintShop.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Layout from "~/components/Layout";
 import PrintOrders from "~/components/PrintShopView/PrintOrders";
 import { getApprovedOrders, getDeliveredOrders, getProcessingOrder } from "../../store/PrintShop/Thunks";
+import { io } from "socket.io-client";
 
 
 
@@ -10,15 +11,26 @@ import { getApprovedOrders, getDeliveredOrders, getProcessingOrder } from "../..
 const PrintShop = () => {
     const dispatch = useDispatch()
     const order = useSelector((state) => state.PrintShop.processingOrders.orders)
+    const socket = io("http://192.168.55.26:4005/");
     useEffect(() => {
+        const token = sessionStorage.getItem('accessToken')
         const getOrders = async () => {
-            const token = sessionStorage.getItem('accessToken')
             await dispatch(getApprovedOrders(token))
             await dispatch(getProcessingOrder(token))
             await dispatch(getDeliveredOrders(token))
             console.log('getting called')
         }
         getOrders()
+
+        socket.on("approvedOrder", async (data) => {
+            if (data.status == 200) {
+                await dispatch(getApprovedOrders(token));
+            }
+        });
+
+        return () => {
+            socket.off("approvedOrder");
+        };
     }, [])
     return (
         <Layout>
