c9a4c242384c850c4d51bf68a1f6e4cf00d396be
Jacob Pixler <jacobpixlerdev@gmail.com>
2024-04-24 11:49:03 -0600

    organized import statements

--- src/pages/current-orders.jsx
+++ src/pages/current-orders.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
 import Layout from "~/components/Layout";
 import OrderCard from "~/components/OrderCard";
-import { useState, useRef } from "react";
-import OrderModal from "~/components/OrderModal";
-import { Menu } from "antd";
 import LeadsOrderApproval from "~/components/LeadsOrderApproval";
+import OrderModal from "~/components/OrderModal";
+import { useEffect } from "react";
+import { useState, useRef } from "react";
+import { Menu } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { useScrollPosition } from "~/hooks/useScrollPosition";
 import { getMyOrders } from "../../store/Orders/thunks";
+import { Space, Table, Tag } from "antd";
+const { Column, ColumnGroup } = Table;
 
 const CurrentOrders = () => {
   const containerRef = useRef(null);
